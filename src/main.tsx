abf7d05989477112a35a1f09301be6fd10e0aba5
Chris Lucas <chris.lucas.dev@gmail.com>
2025-04-04 23:03:38 +1100

    style(fonts): updated the fonts to use Oswald and Playfair Display

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import "@fontsource/playfair-display/400.css";
+import "@fontsource/playfair-display/500.css";
+import "@fontsource/playfair-display/700.css";
+import "@fontsource/oswald/300.css";
+import "@fontsource/oswald/400.css";
+import "@fontsource/oswald/500.css";
+import "@fontsource/oswald/700.css";
 import './index.css'
 import Index from './pages/Index'
 import { BrowserRouter, Route, Routes } from "react-router";
