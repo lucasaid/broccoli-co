4b5555764a75fbba31f2c78f7f084975d50def48
Chris Lucas <chris.lucas.dev@gmail.com>
2025-04-04 22:16:39 +1100

    style(variables): added vars for container padding, added base font size

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,19 +1,19 @@
 import { styled } from "styled-components";
 
 export const HeaderWrapper = styled.header`
+  align-items: center;
+  background-color: var(--reseda-green);
+  border-bottom: 1px solid var(--dark-moss-green);
+  color: var(--pakistan-green);
   display: flex;
   justify-content: start;
-  align-items: center;
-  padding: 10px 20px;
+  padding: var(--container-padding-height) var(--container-padding-width);
   text-transform: uppercase;
-  border-bottom: 1px solid var(--dark-moss-green);
-  background-color: var(--reseda-green);
-  color: var(--pakistan-green);
   @media (prefers-color-scheme: light) {
     background-color: var(--lighter-green);
   }
 `;
 export const Logo = styled.img`
-  margin-right: 10px;
   height: 2rem;
+  margin-right: 10px;
 `;
