d517b9f3535ad45b567efcbf6c0e0dca1d55d41a
Chris Lucas <chris.lucas.dev@gmail.com>
2025-04-06 09:05:11 +1000

    feat(loader): added loader styled component for when form is submitting

---
+++ src/styles/Loader.styles.ts
@@ -0,0 +1,22 @@
+import { keyframes, styled } from "styled-components";
+
+
+const rotation = keyframes`
+  0% {
+      transform: rotate(0deg);
+  }
+  100% {
+      transform: rotate(360deg);
+  }
+`
+
+export const Loader = styled.div`
+width: 48px;
+  height: 48px;
+  border: 5px solid var(--lighter-green);
+  border-bottom-color: var(--reseda-green);
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: ${rotation} 1s linear infinite;
+`;
