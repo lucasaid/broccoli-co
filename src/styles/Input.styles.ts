6536d082010a423cc7cdf29d1a097bc3744605c5
Chris Lucas <chris.lucas.dev@gmail.com>
2025-04-06 09:05:39 +1000

    feat(input): styles for input elements

---
+++ src/styles/Input.styles.ts
@@ -0,0 +1,17 @@
+import {styled} from "styled-components";
+
+export const Input = styled.input`
+  background-color: var(--mindaro);
+  border: none;
+  border-radius: 5px;
+  color: var(--pakistan-green);
+  font-family: Oswald, sans-serif;
+  font-size: var(--fs-sm);
+  font-weight: 600;
+  padding: 10px 20px;
+  width: 100%;
+  max-width: 400px;
+  display: block;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+`;
