ae4fda6957643a4870e50c206097839c488e75b6
Chris Lucas <chris.lucas.dev@gmail.com>
2025-04-06 15:40:11 +1000

    fix(validation): make sure to trim values before posting

--- src/types/forms.ts
+++ src/types/forms.ts
@@ -26,9 +26,9 @@ export type ValidFieldNames = "email" | "name" | "confirmEmail";
 
 export const SignUpSchema: ZodType<FormData> = z
   .object({
-    email: z.string().min(1, "Email is required.").email(),
-    name: z.string().min(1, "Full name is required.").min(3, "Full name must be at least 3 characters."),
-    confirmEmail: z.string().min(1, "Confirm Email is required.").email(),
+    email: z.string().trim().min(1, "Email is required.").email(),
+    name: z.string().trim().min(1, "Full name is required.").min(3, "Full name must be at least 3 characters."),
+    confirmEmail: z.string().trim().min(1, "Confirm Email is required.").email(),
   })
   .refine((data: FormData) => data.email === data.confirmEmail, {
     message: "Emails do not match",
