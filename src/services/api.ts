040b8d908bfa3a5de5ba6edd257e69b2c4a639a1
Chris Lucas <chris.lucas.dev@gmail.com>
2025-04-06 16:22:33 +1000

    refactor(api): added extra error checks

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,22 @@
 import { RequestInvite } from "../types/common";
 
+/**
+ * Sends a POST request to the server to request an invite.
+ *
+ * @param {RequestInvite} param0 - An object containing the name and email for the invite request.
+ * @param {AbortSignal} [signal] - An optional AbortSignal to cancel the request.
+ * @returns {Promise<any>} The response data from the server if the request is successful.
+ * @throws {Error} Throws an error if the request fails with a message from the server.
+ */
+
 export const requestInvite = async (
   { name, email }: RequestInvite,
   signal?: AbortSignal
-) => {
+): Promise<any> => {
+  if (!name || !email) {
+    throw new Error("Name and email are required");
+  }
+
   const response = await fetch(
     "https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth",
     {
@@ -18,6 +31,9 @@ export const requestInvite = async (
 
   if (!response.ok) {
     const error = await response.json();
+    if (!error || !error.errorMessage) {
+      throw new Error("Unknown error");
+    }
     throw new Error(error.errorMessage);
   }
 
