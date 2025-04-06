18e185ec29f488887b5e57e329c05f51fa636cc6
Chris Lucas <chris.lucas.dev@gmail.com>
2025-04-06 13:36:38 +1000

    feat(constants): added constant for company name

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,19 +2,20 @@ import { Footer } from "../styles/Footer.styles";
 import { Header, Logo } from "../styles/Header.styles";
 import { Container, Content } from "./Layout.styles";
 import broccoliLogo from '../assets/broccoli.png'
+import { COMPANY_NAME } from "../utils/constants";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Container>
       <Header>
-        <Logo src={broccoliLogo} alt="Broccoli & Co Logo" />
-        <h1>Broccoli & Co</h1>
+        <Logo src={broccoliLogo} alt={`${COMPANY_NAME} Logo`} />
+        <h1>{COMPANY_NAME}</h1>
       </Header>
       <Content>{children}</Content>
       <Footer>
         <span>Made with ♥ in Melbourne.</span>
         <br />
         <span>
-          &copy; {new Date().getFullYear()} Broccoli & Co. All rights reserved.
+          &copy; {new Date().getFullYear()} {COMPANY_NAME} All rights reserved.
         </span>
       </Footer>
     </Container>
